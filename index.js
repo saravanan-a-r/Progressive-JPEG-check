72c9a839c2247ed5c166f9d36dd6506ea6df34e2
System Administrator <Saravanan A R>
2019-02-04 03:59:33 +0530

    Integration test case added

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const progressiveChecker = require('./ProgressiveChecker');
+const progressiveChecker = require('./src/ProgressiveChecker');
 
-progressiveChecker.setImage('./test/test1.jpeg')
+progressiveChecker.setImage('./test/integ-testing/src/test1.jpeg')
     .isProgressive()
     .then( (result) => {
         console.log('test1.jpeg :: isProgressive() : ' + result);
     });
 
-progressiveChecker.setImage('./test/test1.jpeg')
+progressiveChecker.setImage('./test/integ-testing/src/test1.jpeg')
     .noOfScan()
     .then( (result) => {
         console.log('test1.jpeg :: noOfScan() : ' + result);
     });
 
-progressiveChecker.setImage('./test/test2.jpeg')
+progressiveChecker.setImage('./test/integ-testing/src/test2.jpeg')
     .isProgressive()
     .then( (result) => {
         console.log('test2.jpeg :: isProgressive() : ' + result);
     });
     
-progressiveChecker.setImage('./test/test2.jpeg')
+progressiveChecker.setImage('./test/integ-testing/src/test2.jpeg')
     .noOfScan()
     .then( (result) => {
         console.log('test2.jpeg :: noOfScan() : ' + result);
---
+++ test/integ-testing/src/progressiveChecker.test.js
@@ -0,0 +1,37 @@
+const progressiveChecker = require('../../../src/ProgressiveChecker');
+const mocha = require('mocha');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Integration testing', () => {
+
+    context('Scan count', () => {
+
+        it('Should return scan count as 10', async () => {
+            let expectedScanCount = 10;
+            let actualScanCount = await progressiveChecker.setImage('./test/integ-testing/src/test1.jpeg').noOfScan();
+            expect(actualScanCount).to.equal(expectedScanCount);
+        });
+
+        it('Should return scan count as 1', async () => {
+            let expectedScanCount = 1;
+            let actualScanCount = await progressiveChecker.setImage('./test/integ-testing/src/test2.jpeg').noOfScan();
+            expect(actualScanCount).to.equal(expectedScanCount);
+        });
+    });
+
+    context('Progressive checkness', () => {
+
+        it('Should return isProgressive : true', async () => {
+            let expectedResult = true;
+            let actualResult = await progressiveChecker.setImage('./test/integ-testing/src/test1.jpeg').isProgressive();
+            expect(actualResult).to.equal(expectedResult);
+        });
+
+        it('Should return isProgressive : false', async () => {
+            let expectedResult = false;
+            let actualResult = await progressiveChecker.setImage('./test/integ-testing/src/test2.jpeg').isProgressive();
+            expect(actualResult).to.equal(expectedResult);
+        });
+    });
+});
